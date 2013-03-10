26b3d1be83101baaae0937a61a298702d4324e26
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-03-10 21:53:09 +0100

    Adding comments to collection, models and views

--- js/app/views/project.js
+++ js/app/views/project.js
@@ -5,19 +5,49 @@
 ], function( $, Backbone, Handlebars ) {
 
   var ProjectView = Backbone.View.extend({
-    template: Handlebars.compile($('#project-list-template').html()),
-    
+
+    /**
+     * Load template and compile handlebars template.
+     */
+
+     template: Handlebars.compile($('#project-list-template').html()),
+
+    /**
+     * Bind events.
+     */
+
     events: {
       'click .delete-project': 'delete'
     },
-    
+
+    /**
+     * Render `ProjectView`.
+     */
+
     render: function() {
       this.$el.html(this.template(this));
       return this;
     },
-    
+
+    /**
+     * Get project title.
+     *
+     * @return {String}
+     */
+
     title: function() { return this.model.get('title'); },
+
+    /**
+     * Get project id.
+     *
+     * @return {String|Int}
+     */
+
     id: function () { return this.model.get('id'); },
+
+    /**
+     * Delete project.
+     */
 
     delete: function() {
       this.model.destroy();
--- js/app/views/statuses.js
+++ js/app/views/statuses.js
@@ -7,27 +7,65 @@ define([
 ], function( $, Backbone, Handlebars, statuses, StatusView ) {
 
   var StatusesView = Backbone.View.extend({
+
+    /**
+     * Load template and compile handlebars template.
+     */
+
     template: Handlebars.compile($('#status-template').html()),
+
+    /**
+     * Initialize `ProjectsView`.
+     */
+
     initialize: function() {
       this.statuses = statuses;
       this.statuses.on('all', this.render, this);
     },
+
+    /**
+     * Bind events.
+     */
+
     events: {
       'click #addstatus': 'addStatus'
     },
+
+    /**
+     * Render `StatusesView`.
+     */
+
     render: function() {
       this.$el.html(this.template(this));
       this.statuses.each(this.populateStatusList, this);
       return this;
     },
 
+    /**
+     * Populate status list.
+     */
+
     populateStatusList: function(status) {
       var view = new StatusView({model: status});
       this.$('#status-list').append(view.render().el);
     },
+
+    /**
+     * Get status count.
+     *
+     * @return {Int}
+     */
+
     statusCount: function() {
       return this.statuses.length;
     },
+
+    /**
+     * Add status.
+     *
+     * @param {Object} event
+     */
+
     addStatus: function(event) {
       event.preventDefault();
       this.statuses.create({
